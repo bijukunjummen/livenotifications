06bd05fc6679485d3513b768e5c411af3b0a0caf
Biju Kunjummen <biju.kunjummen@gmail.com>
2022-12-27 22:24:53 -0800

    Misc lib upgrades

--- src/main/kotlin/org/bk/notification/config/BigtableProperties.kt
+++ src/main/kotlin/org/bk/notification/config/BigtableProperties.kt
@@ -1,11 +1,9 @@
 package org.bk.notification.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConfigurationProperties(prefix = "bigtable")
-@ConstructorBinding
-class BigtableProperties(
+data class BigtableProperties(
     val instanceId: String = "bus-instance",
     val emulatorPort: Int = 0,
     val projectId: String = "project-id"
--- src/main/kotlin/org/bk/notification/config/SpannerProperties.kt
+++ src/main/kotlin/org/bk/notification/config/SpannerProperties.kt
@@ -1,11 +1,8 @@
 package org.bk.notification.config
 
-import com.google.cloud.spanner.InstanceConfigId
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConfigurationProperties(prefix = "spanner")
-@ConstructorBinding
 data class SpannerProperties(
     val instanceId: String = "",
     val database: String = "",
